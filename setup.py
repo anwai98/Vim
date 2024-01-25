1ba0915ddb71c63e1249fde1ae0165f6a014fa5f
anwai98 <anwai.archit@gmail.com>
2024-01-25 20:52:09 +0100

    Create vim module

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='vim',
+    version='0.0.1',
+    description='For Vision Mamba',
+    author='...',
+    author_email='...',
+    url='...',
+    packages=['vim'],
+)
