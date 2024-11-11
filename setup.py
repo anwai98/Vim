b2df980a3d8afe446552e1dd42ccc7d254dfd8f0
anwai98 <anwai.archit@gmail.com>
2024-11-11 13:35:45 +0100

    Add Vim module installation setup and minor import-level changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='vim',
+    version='0.0.1',
+    description='Installation for Vision Mamba',
+    packages=['vim'],
+)
